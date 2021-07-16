d09c8ea30df5ffcc9c1dcd95a4d2fa3567990ff1
walker015 <walker.alvesmarques@gmail.com>
2021-07-16 18:31:36 -0300

    submit, janela de cadastro, local storage

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
+const form = document.getElementById('formulario')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let name = document.getElementById('name').value
+    let email = document.getElementById('email').value
+    let data = {
+        name,
+        email
+    }
+    let converData = JSON.stringify(data)
+
+    localStorage.setItem('lead', converData)
+})
+
 document.getElementById("section-one").addEventListener("click", function () {
-    document.getElementById("section-one").classList.remove('background-section');
-    document.getElementById("section-one").classList.add('background-section-mod');
-    document.getElementById("window").style.display = "flex";
-    const section = document.getElementById("section-one");
-    const sectionAproveitar = document.getElementById("aproveitar");
-    section.removeChild(sectionAproveitar);
+    document.getElementById("section-one").classList.remove('background-section')
+    document.getElementById("section-one").classList.add('background-section-mod')
+    document.getElementById("window").style.display = "flex"
+    const section = document.getElementById("section-one")
+    const sectionAproveitar = document.getElementById("aproveitar")
+    section.removeChild(sectionAproveitar)
 });
 
