65f93214c4da093afd2651bfce30c83851185b9f
walker015 <walker.alvesmarques@gmail.com>
2021-07-18 17:33:51 -0300

    first media

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('formulario')
+const mediaQueryList = window.matchMedia("(max-width: 700px)")
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -15,10 +16,18 @@ form.addEventListener('submit', (e) => {
 
 document.getElementById("section-one").addEventListener("click", function () {
     document.getElementById("section-one").style.display = "none"
-    document.getElementById("section-two").style.display = "flex"
-    document.getElementById("window").style.display = "flex"
     const section = document.getElementById("section-one")
     const sectionAproveitar = document.getElementById("aproveitar")
     section.removeChild(sectionAproveitar)
+
+    if (mediaQueryList.matches) {
+        document.getElementById("section-two").style.display = "grid"
+        document.getElementById("window").style.display = "grid"
+        document.getElementById("div-one").style.display = "grid"
+        document.getElementById("section-two").classList.remove("section")
+    } else {
+        document.getElementById("section-two").style.display = "flex"
+        document.getElementById("window").style.display = "flex"
+    }
 });
 
